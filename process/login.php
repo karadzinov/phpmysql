05a8f7164edd958502118072bc1019046b2205ec
martin <martin@pingdevs.com>
2022-12-19 19:56:34 +0100

    Adding config folder with files

---
+++ process/login.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+
+
+if(isset($_POST['email']) & !empty($_POST['email'])) {
+    $email = $_POST['email'];
+} else {
+    $email = '';
+}
+
+if(isset($_POST['password']) && !empty($_POST['password'])) {
+    $password = $_POST['password'];
+} else {
+    $password = '';
+}
+
+
+
+
+
+
